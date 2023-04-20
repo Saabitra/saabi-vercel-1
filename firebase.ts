792384a72a069046528d639c137fe6cbe8871016
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-20 21:53:20 +0100

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -9,16 +9,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDi-36tTaRfn6pHldgpznqtV3I-NuXrZk8",
-  authDomain: "saabi-2.firebaseapp.com",
-  projectId: "saabi-2",
-  storageBucket: "saabi-2.appspot.com",
-  messagingSenderId: "168732431975",
-  appId: "1:168732431975:web:d41fcf9bd6bf75102866c2"
+  apiKey: "AIzaSyA7lwpsCci5vOGoFNMtA0tkP4do_kSTklQ",
+  authDomain: "saabi-vercel.firebaseapp.com",
+  projectId: "saabi-vercel",
+  storageBucket: "saabi-vercel.appspot.com",
+  messagingSenderId: "207183898392",
+  appId: "1:207183898392:web:135aeaa19c2696e4d3e06d"
 };
 
 // Initialize Firebase
-const app = getApps.length ? getApp() : initializeApp(firebaseConfig);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
