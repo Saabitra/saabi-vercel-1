af372bca20da301355a3c3cc8d93007063b9ff96
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-21 14:46:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
       <body>
 
 
-      <SessionProvider session={session}>
+<!--       <SessionProvider session={session}> -->
 <!--         {!session ? (
             <Login />
         ) : ( -->
